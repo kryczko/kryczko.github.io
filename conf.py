a04f7f6987e5da945f21665e251e3aa1473aa50b
Kevin Ryczko <kryczko@boltzmann.local>
2025-12-11 19:50:01 -0500

    updated title

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@
 project = 'Kevin Ryczko, PhD'
 copyright = '2025, Kevin Ryczko'
 author = 'Kevin Ryczko'
-release = '1.0'
+release = ''
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
